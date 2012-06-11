8e9e505339e584d1430edf7c590991610e23d375
Kabindra Shrestha <shrestha_kabin@yahoo.com>
2012-06-11 12:18:21 +0545

    conflict resolved

--- beans/src/main/java/org/sola/clients/beans/referencedata/VdcBean.java
+++ beans/src/main/java/org/sola/clients/beans/referencedata/VdcBean.java
@@ -16,7 +16,6 @@
 package org.sola.clients.beans.referencedata;
 
 import org.sola.clients.beans.AbstractCodeBean;
-import org.sola.webservices.transferobjects.referencedata.VdcTO;
 /**
  *
  * @author KumarKhadka
@@ -24,21 +23,13 @@ import org.sola.webservices.transferobjects.referencedata.VdcTO;
 public class VdcBean extends AbstractCodeBean {
 
     public static final String DISTRICT_CODE_PROPERTY = "districtCode";
-<<<<<<< HEAD
-    private String districtCode;
 
-    public VdcBean() {
-        super();
-    }
-
-=======
     private String districtCode;  
     
     public VdcBean(){
         super();
     }
     
->>>>>>> nepal_custom
     public String getDistrictCode() {
         return districtCode;
     }
