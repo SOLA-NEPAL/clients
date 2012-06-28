6abaf92ccb0bbcc0eb1885de175c0e5b110c6655
Dinesh Tuitui <thesunonthesky@hotmail.com>
2012-06-28 15:23:24 +0545

    added Restriction and release  lookup tables managements

---
+++ beans/src/main/java/org/sola/clients/beans/referencedata/RestrictionOfficeBean.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2012 Food and Agriculture Organization of the United Nations (FAO).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sola.clients.beans.referencedata;
+
+import org.sola.clients.beans.AbstractCodeBean;
+
+/**
+ *
+ * @author Dinesh
+ */
+public class RestrictionOfficeBean extends AbstractCodeBean {
+
+    public static final String CODE_CANCEL = "cancel";
+    public static final String CODE_NEW = "new";
+    public static final String CODE_VARY = "vary";
+
+    public RestrictionOfficeBean() {
+        super();
+    }
+    
+}
